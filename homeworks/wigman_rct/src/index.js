29bc73f8222d8cc17562e22e732c6a0cba9ebac7
Joemontas <joseart80@gmail.com>
2018-05-26 19:21:34 -0400

    Updates 05-26-18

---
+++ homeworks/wigman_rct/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import ParentComponent from './parent-component';
+
+ReactDOM.render(<App />, document.getElementById('root'));
